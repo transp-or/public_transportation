621dfd4aab45e5aa70987db6da2ae03f66ef9bb9
Michel Bierlaire <michel.bierlaire@epfl.ch>
2026-07-06 15:45:38 +0200

    Old directory

--- src/public_transportation/bayesian_estimation/__init__.py
+++
@@ -1,7 +0,0 @@
-from .core_vi import run_vi
-from .config import VIConfig
-from .persistence import save_vi_result
-from .diagnostics import compute_all_diagnostics
-from .report_data import build_vi_report_data
-from .report_html import generate_vi_report_html
-from .report_plots import generate_vi_report_plots
